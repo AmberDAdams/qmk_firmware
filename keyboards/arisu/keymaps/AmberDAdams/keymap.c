d89f7434b2edaa180acc7ec86bd624dd092e3b34
Amber <amberdadams1@gmail.com>
2021-07-18 22:28:04 +0100

    add colemak layer to arisu

--- keyboards/arisu/keymaps/AmberDAdams/keymap.c
+++ keyboards/arisu/keymaps/AmberDAdams/keymap.c
@@ -17,17 +17,27 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    MO(1),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NUBS, KC_DEL,  KC_HOME,
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC, KC_BSPC,          KC_END,
+    MO(2),   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NUBS, KC_DEL,  KC_HOME,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_SLSH, KC_BSPC,          KC_END,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_QUOT, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,                    KC_GESC,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,          KC_UP,
-    KC_LCTL,          KC_LALT,          KC_SPC,  KC_LGUI,          KC_SPC,           KC_RALT,                   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL,          KC_LALT,          KC_SPC,  KC_APP,           KC_SPC,           KC_LWIN,                   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [1] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,  _______, _______, KC_PGDN,
-    KC_HAEN, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGDN,
-    KC_HANJ, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,
+    // colemak, but done differentially versus qwerty (layer 0) in an attempt to save controller space
+    // likely to flip these around in future so that colemak is base
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, KC_F,    KC_P,    KC_G,    _______, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______, _______,          _______,
+    _______, _______, KC_R,    KC_S,    KC_T,    KC_D,    _______, _______, KC_N,    KC_E,    KC_I,    KC_O,    _______,                   _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_K,    _______, _______, _______, _______,          _______,
+    _______,          _______,          _______, _______,          _______,          _______,                   _______, _______, _______
+  ),
+
+  [2] = LAYOUT(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,  _______, RESET, KC_PGUP,
+    TO(0),   _______, _______, _______, _______, _______, KC_RBRC, _______, _______, _______, _______, _______, _______, _______,        KC_PGDN,
+    TO(1),   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                 _______,
     _______, _______, _______, _______, _______, KC_NUHS, _______, _______, _______, _______, _______, _______,          _______,
     _______,          _______,          _______, _______,          _______,          _______,                   _______, _______, _______
   )
